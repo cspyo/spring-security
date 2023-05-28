359674f24b85887a9735ecfbcb0d43b20c646a5a
최승표 <cspyo98@naver.com>
2023-05-29 01:24:27 +0900

    Feat: User 모델 생성

---
+++ src/main/java/com/spring/security/model/User.java
@@ -0,0 +1,29 @@
+package com.spring.security.model;
+
+import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+}
